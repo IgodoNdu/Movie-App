472c1de02f17060628e1527174e6b6c9a159d035
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-13 00:04:00 +0100

    Update Header.js
    Update the header component, with links for redirects, App logo, and user image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+//for links/redirects
+import { Link } from 'react-router-dom';
+import user from '../../images/user.png';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='header'>
+      <Link to='/'>
+        <div className='logo'>Movie App</div>
+      </Link>
+      <div className='user-logo'>
+        <img src={user} alt='user' />
+      </div>
     </div>
   )
 }
 
-export default Header
+export default Header;
