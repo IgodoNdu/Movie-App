f7439b5a19709997ce491d5aa5950d6bb9026717
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-12 23:37:11 +0100

    Update App.js
    Update the App component, and import the requisite components for page navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+//For Routing purposes
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//Import components (Imported individually here, will implement a cleaner and efficient approach)
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>Movie Rating App</h3>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
