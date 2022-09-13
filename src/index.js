51fe2a351cc3c92e0915e13f72a584f83c35a0a5
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-13 01:39:07 +0100

    Make the store available to app components
    Now providing our store to the components via the index.js file. making use of the Provider component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//Now provide the created store to component(s)
+import { Provider } from 'react-redux';
+//Import the store to be mad available/provided to component(s)
+import { store } from './features/store';
 
 import App from './App';
 
@@ -7,7 +11,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/**Now provide the store as a prop to provider, by wrapping around the app component, thus now available to every component through APP */}
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
