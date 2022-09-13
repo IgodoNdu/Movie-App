f52630bbf52be8d4bf5942be3333687ddfcc4726
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-13 03:13:55 +0100

    Update MovieListing.jsx
    Getting all the movies listing from the store with useelector, and the getAllMovies function created earlier in movieSlice.js

--- src/components/MovieListing/MovieListing.jsx
+++ src/components/MovieListing/MovieListing.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+//To get all the movies from store, we'll need the useSelector & the getAllMovies function from movieSlice
+import { useSelector } from 'react-redux'
+import { getAllMovies } from '../../features/movies/movieSlice'
 
 const MovieListing = () => {
+  //Getting all the movies from the store
+  const movies = useSelector(getAllMovies);
+  console.log(movies);
   return (
     <div>
       Movie Listing
