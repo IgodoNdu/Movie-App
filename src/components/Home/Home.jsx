ad11213177427cec531bb460c30bfb456f9c9cd0
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-13 01:23:19 +0100

    Fetch data from OMDB API
    Fetched the data from OMDB via the useEffect Hook, and assigned APIKey. Checkout OMDB endpoint. Had to hard-code a search-term for now, will make it dynamic later. The received data will be saved to our store via redux

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+//Import MovieListing Component for use
+import MovieListing from '../MovieListing/MovieListing';
+//import base url & API Key fro OMDB movies
+import movieApi from '../apis/movieApi';
+import { APIKey } from '../apis/MovieApiKey';
 
 const Home = () => {
+  //fetch data and save to our redux store. Hence we'll need to make API Call
+  //using the useEffect Hook to make API call (import useEffect)
+  useEffect(() => {
+    //create a function for the fetch movies (async func)
+    //create a var for the search term
+    const movieSearchText = "Harry"
+    const fetchMovies = async () => {
+      //for the response (import the base url & API Key) include a search term with 's': see omdb endpoints
+      const response = await movieApi.get(`?apikey=${APIKey}&s=${movieSearchText}&type=movie`).catch((err) => {
+        console.log("Err :", err)
+      });
+      console.log("API Response ", response);
+    };
+    //Now call the function (fetchMovies) inside the useEffect (Here, i'll call once, therefore will add the empty dependency)
+    fetchMovies();
+  }, []);
+
   return (
     <div>
-      Home
+      <div className='banner-img'></div>
+      <MovieListing />
     </div>
   )
 }
