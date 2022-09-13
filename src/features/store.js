8c04aa8c40ba3d5d1b9922de31beca713c9c7783
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-13 02:21:03 +0100

    Adding Reducer to the store
    Now add the reducer to the store

--- src/features/store.js
+++ src/features/store.js
@@ -1,7 +1,9 @@
 //Configure the store to save the received data from API calls
 //Here we'll be using Redux tool-kit, which makes it easy to work with Redux, as against the traditional style
 import { configureStore } from "@reduxjs/toolkit";
+//Now import the reducer created in the movieSlice, & add to the configurestore option
+import moviesReducer from "./movies/movieSlice";
 
 export const store = configureStore({
-    reducer: {},
-})
+    reducer: {moviesReducer},
+});
